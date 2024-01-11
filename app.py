7cf91e462844ac67d93c18abec9fae9c723cea9c
StephenJBall <stephenjb94@gmail.com>
2024-01-11 16:22:47 +0000

    Finished jinja template for html pages

--- app.py
+++ app.py
@@ -1,16 +1,36 @@
 import os
 
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, redirect, render_template, request
 from flask_session import Session
 import sqlite3
 
 app = Flask(__name__)
 
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+app.config['SECRET KEY'] = 'secretkey'
 
 @app.route("/")
 def index():
     if request.method == "GET":
         return render_template("index.html")
+
+@app.route("/playerbase")
+def playerbase():
+    if request.method == "GET":
+        return render_template("playerbase.html")
+    
+@app.route("/matchreports")
+def matchreports():
+    if request.method == "GET":
+        return render_template("/matchreports.html")
+    
+@app.route("/injuryreports")
+def injuryreports():
+    if request.method == "GET":
+        return render_template("/injuryreports.html")
+
+@app.route("/contracts")
+def contracts():
+    if request.method == "GET":
+        return render_template("/contracts.html") 
+    
+
